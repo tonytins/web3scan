69e64df2d8399b77426ed9aa5c871b05c79a734e
Tony Bark <35226681+tonytins@users.noreply.github.com>
2022-07-22 12:48:01 -0400

    Updated dependencies Had to do some rewrites. reqwest changed a lot since I last used it.

--- examples/web3.rs
+++ examples/web3.rs
@@ -1,14 +1,17 @@
+use reqwest::Error;
 use web3scan::Web3;
 
-pub fn main() {
+#[tokio::main]
+pub async fn main() -> Result<(), Error> {
     let address = "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B";
-    let err_msg = "Parsing error";
     let network = Web3::new("api.etherscan.io");
-    let balance = network.get_balance(address).expect(err_msg);
-    let total_supply = network.get_total_supply().expect(err_msg);
-    let last_price = network.get_last_price().expect(err_msg);
+    let balance = network.get_balance(address).await?;
+    let total_supply = network.get_total_supply().await?;
+    let last_price = network.get_last_price().await?;
 
     println!("Balance: {:#?}", balance);
     println!("Total supply: {:#?}", total_supply);
     println!("{:#?}", last_price);
+
+    Ok(())
 }
